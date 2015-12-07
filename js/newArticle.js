28d6bdc698dedbd5e363c0867a05451c6613c779
Sabrina Tee <sabrinatee@me.com>
2015-12-07 10:45:02 -0800

    Working highlight.js

--- js/newArticle.js
+++ js/newArticle.js
@@ -1,14 +1,14 @@
 $('#write').on('keyup', function(){
-  var $articlePreview = '<h1>'+ $('#article-title').val() + '</h1>';
-  $articlePreview += '<h5>' + $('#article-author').val() + '</h5>';
-  $articlePreview += '<h5><a>' + $('#article-author-url').val() + '</a></h5>';
-  $articlePreview += '<h5>Published on ' + new Date() + '</h5>';
-  $articlePreview += '<h6>Category: ' + $('#article-category').val() + '</h6>';
-  $articlePreview += '```' + $('#article-body').val() + '```';
-  $('#articlesPreview').html(marked($articlePreview));
+  var articlePreview = '<h1>'+ $('#article-title').val() + '</h1>';
+  articlePreview += '<h5>' + $('#article-author').val() + '</h5>';
+  articlePreview += '<h5><a>' + $('#article-author-url').val() + '</a></h5>';
+  articlePreview += '<h5>Published on ' + new Date() + '</h5>';
+  articlePreview += '<h6>Category: ' + $('#article-category').val() + '</h6>';
+  articlePreview += '<pre><code>' + $('#article-body').val() + '</code></pre>';
+  $('#articlesPreview').html(marked(articlePreview));
 
-  $('pre code').addClass('html');
   $('pre code').each(function(i, block) {
+    $('pre code').addClass('html');
     hljs.highlightBlock(block);
   });
 });
