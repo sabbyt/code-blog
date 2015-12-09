24beb717848b535d5496c246b737af5b634dea50
Sabrina Tee <sabrinatee@me.com>
2015-12-09 00:04:42 -0800

    Working on avg word count by author

--- js/stats.js
+++ js/stats.js
@@ -2,6 +2,7 @@ var stats = {};
 stats.author = [];
 stats.words = [];
 var wordCount;
+var uniqueAuthor;
 
 $.getJSON('js/hackerIpsum.json', function(data){
   stats.data = data;
@@ -11,11 +12,11 @@ $.getJSON('js/hackerIpsum.json', function(data){
     stats.totalAuthors();
     stats.totalWords();
     stats.avgOverall();
+    stats.wordsByAuthor();
 });
 
 stats.avgOverall = function() {
   var averageWord = wordCount.reduce(stats.count)/stats.data.length;
-  console.log(averageWord);
   $('#avgOverall').html('Average word length across all posts: ' + averageWord + ' words');
 };
 
@@ -35,8 +36,8 @@ stats.totalArticles = function() {
 
 stats.totalAuthors = function() {
   stats.author = stats.pluck('author', stats.data);
-  var author = $.unique(stats.author);
-  $('#totalAuthors').html('Total authors: ' + author.length);
+  uniqueAuthor = $.unique(stats.author);
+  $('#totalAuthors').html('Total authors: ' + uniqueAuthor.length);
 };
 
 stats.totalWords = function() {
@@ -46,4 +47,14 @@ stats.totalWords = function() {
   });
   var total = wordCount.reduce(stats.count);
   $('#totalWords').html('Total words: ' + total);
+};
+
+stats.wordsByAuthor = function() {
+  uniqueAuthor.forEach(function(element, index){
+    var findAuthor = stats.data;
+    console.log(uniqueAuthor);
+    console.log(findAuthor);
+    findAuthor.match(uniqueAuthor);
+    console.log(findAuthor);
+  });
 };
